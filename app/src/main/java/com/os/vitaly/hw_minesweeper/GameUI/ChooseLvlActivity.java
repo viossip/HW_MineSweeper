d061179351e4c498e38f66dd878d91e0e17172d2
Vitaly <viossip@gmail.com>
2017-08-23 21:53:11 +0300

    Main and ChooseLVL avtivities and buttons

--- app/src/main/java/com/os/vitaly/hw_minesweeper/GameUI/ChooseLvlActivity.java
+++ app/src/main/java/com/os/vitaly/hw_minesweeper/GameUI/ChooseLvlActivity.java
@@ -1,15 +1,34 @@
 package com.os.vitaly.hw_minesweeper.GameUI;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.os.vitaly.hw_minesweeper.R;
 
+/*public enum Level {
+    Easy("Easy"), Normal("Normal"), Hard("Hard");
+    */
 public class ChooseLvlActivity extends AppCompatActivity {
+
+    private ImageButton buttonBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_lvl);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        buttonBack = (ImageButton)findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/os/vitaly/hw_minesweeper/GameUI/HelpActivity.java
+++ app/src/main/java/com/os/vitaly/hw_minesweeper/GameUI/HelpActivity.java
@@ -1,15 +1,20 @@
 package com.os.vitaly.hw_minesweeper.GameUI;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.os.vitaly.hw_minesweeper.R;
 
 public class HelpActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
     }
 }
