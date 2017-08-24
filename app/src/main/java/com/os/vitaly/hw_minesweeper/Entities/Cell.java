3e585bfd1fec055f25cee02a3baac8552bd17cf8
Vitaly <viossip@gmail.com>
2017-08-24 20:00:17 +0300

    entities in proccess

--- app/src/main/java/com/os/vitaly/hw_minesweeper/Entities/Cell.java
+++ app/src/main/java/com/os/vitaly/hw_minesweeper/Entities/Cell.java
@@ -1,13 +1,129 @@
 package com.os.vitaly.hw_minesweeper.Entities;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+
+import com.os.vitaly.hw_minesweeper.R;
+
 /**
  * Created by Vitaly on 12.08.2017.
  */
 
-public class Cell {
+public class Cell extends BaseCell implements View.OnClickListener , View.OnLongClickListener{
 
-    public enum CellValue{
-        Empty, Covered, Mine, Flag, One, Two, Three, Four, Five, Six, Seven, Eight, Nine;
+    public Cell(Context context , int x , int y ){
+        super(context);
+
+        setPosition(x,y);
+
+        setOnClickListener(this);
+        setOnLongClickListener(this);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+
+    @Override
+    public void onClick(View v) {
+        GameRunner.getInstance().click( getXPos(), getYPos() );
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        GameRunner.getInstance().flag( getXPos() , getYPos() );
+
+        return true;
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.d("Minesweeper" , "Cell::onDraw");
+        drawButton(canvas);
+
+        if( isFlagged() ){
+            drawFlag(canvas);
+        }else if( isRevealed() && isBomb() && !isClicked() ){
+            drawNormalBomb(canvas);
+        }else {
+            if( isClicked() ){
+                if( getValue() == -1 ){
+                    drawBombExploded(canvas);
+                }else {
+                    drawNumber(canvas);
+                }
+            }else{
+                drawButton(canvas);
+            }
+        }
+    }
+
+    private void drawBombExploded(Canvas canvas ){
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.bomb_exploded);
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
+    }
+
+    private void drawFlag( Canvas canvas ){
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.flag);
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
+    }
+
+    private void drawButton(Canvas canvas ){
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.button);
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
+    }
+
+    private void drawNormalBomb(Canvas canvas ){
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.zero);
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
+    }
+
+    private void drawNumber( Canvas canvas ){
+        Drawable drawable = null;
+
+        switch (getValue() ){
+            case 0:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.zero);
+                break;
+            case 1:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.one);
+                break;
+            case 2:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.two);
+                break;
+            case 3:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.three);
+                break;
+            case 4:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.four);
+                break;
+            case 5:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.five);
+                break;
+            case 6:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.six);
+                break;
+            case 7:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.seven);
+                break;
+            case 8:
+                drawable = ContextCompat.getDrawable(getContext(), R.drawable.eight);
+                break;
+        }
+
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
     }
 
 
