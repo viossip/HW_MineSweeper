74c67e8e532445ce6aa67c2478277b157ea3e1f1
ilya221990 <ilya160848@gmail.com>
2017-08-24 20:24:28 +0300

    Merge branch 'master' of https://github.com/viossip/HW_MineSweeper
    # Conflicts:
    #	app/src/main/java/com/os/vitaly/hw_minesweeper/Controls/GameRunner.java

--- app/src/main/java/com/os/vitaly/hw_minesweeper/Entities/Cell.java
+++ app/src/main/java/com/os/vitaly/hw_minesweeper/Entities/Cell.java
@@ -1,14 +1,26 @@
 package com.os.vitaly.hw_minesweeper.Entities;
 
+import android.content.Context;
+import android.view.View;
+
 /**
  * Created by Vitaly on 12.08.2017.
  */
 
-public class Cell {
+public class  Cell extends View{
+
+    public void setClicked() {
+    }
+
+    public int getValue() {
+        return value;
+    }
 
     public enum CellValue{
         Empty, Covered, Mine, Flag, One, Two, Three, Four, Five, Six, Seven, Eight, Nine;
     }
 
-
+    public Cell(Context context ){
+        super(context);
+    }
 }
