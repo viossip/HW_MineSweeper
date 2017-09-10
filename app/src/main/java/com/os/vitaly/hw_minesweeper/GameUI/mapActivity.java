6f4ccb72f48e41980503895afb2b4a7bb410bc4f
ilya221990 <ilya160848@gmail.com>
2017-09-10 15:20:41 +0300

    map added ,gps

---
+++ app/src/main/java/com/os/vitaly/hw_minesweeper/GameUI/mapActivity.java
@@ -0,0 +1,50 @@
+package com.os.vitaly.hw_minesweeper.GameUI;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.os.vitaly.hw_minesweeper.R;
+
+/**
+ * Created by ilya on 09/09/2017.
+ */
+
+public class mapActivity extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreate(LayoutInflater inflater, ViewGroup container ,Bundle savedInstanceState){
+        mView=inflater.inflate(R.layout.gps,container,false);
+        return mView;
+    }
+    public void  onViewCreated(View view,Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        mMapView= (MapView) mView.findViewById(R.id.map);
+        if (mMapView!=null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+    }
+}
